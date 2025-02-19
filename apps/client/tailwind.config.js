6baa3a28c5e49d1209edbf94e5880a7417e30f15
woogie0303 <rkdehddnr15123@gmail.com>
2025-02-19 18:34:28 +0900

    chore: init

---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans KR", "sans-serif"],
+      },
+      boxShadow: {
+        tagBox: "0 1px 3px rgba(0, 0, 0, 0.25)",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
