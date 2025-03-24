214dfcd5edd9b8a64fecf42486646b8282b892bb
woogie0303 <rkdehddnr15123@gmail.com>
2025-03-24 15:21:10 +0900

    fix: cors 문제 해결 (back)

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
